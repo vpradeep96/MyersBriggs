8fe5c700737fadb85bc255f3482d19cb25d823a7
Sai Tadikonda <saitadikonda48@gmail.com>
2019-04-21 19:31:34 -0400

    UI Changes

--- pages/views.py
+++ pages/views.py
@@ -24,10 +24,6 @@ def home_view(request, *args, **kwargs):	# *args, **kwargs
 			text_field_data = textInputField.cleaned_data.get('text_field')
 			if text_field_data:
 				twitter_search = False
-		if twitterHandleField.is_valid():
-			twitter_field_data = twitterHandleField.cleaned_data.get('twitter_handle')
-			if twitter_field_data:
-				twitter_search = True
 		if extraversionField.is_valid():
 			extraversion_data = extraversionField.cleaned_data.get('extraversion_field')
 		if intuitionField.is_valid():
@@ -37,6 +33,12 @@ def home_view(request, *args, **kwargs):	# *args, **kwargs
 		if perceptionField.is_valid():
 			perception_data = perceptionField.cleaned_data.get('perception_field')
 			print(perception_data)
+
+
+		if twitterHandleField.is_valid():
+			twitter_field_data = twitterHandleField.cleaned_data.get('twitter_handle')
+			if twitter_field_data:
+				twitter_search = True
 
 
 
