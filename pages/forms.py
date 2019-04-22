614a122e620ea5952ca164e6acbe7ef66f536bb6
Sai Tadikonda <saitadikonda48@gmail.com>
2019-04-21 20:49:24 -0400

    UI updates

--- pages/forms.py
+++ pages/forms.py
@@ -17,22 +17,33 @@ from django import forms
 
 class TextInputField(forms.Form):
 	text_field = forms.CharField(widget=forms.Textarea, label='', required=None)
+	
+	OPTIONS_E = (("E", "E"),("I", "I"),)
+	extraversion_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS_E, label='Extraversion/Introversion')
+	OPTIONS_S = (("S", "S"),("N", "N"),)
+	intuition_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS_S, label='Sensing/Intuition')
+	OPTIONS_T = (("T", "T"),("F", "F"),)
+	feeling_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS_T, label='Thinking/Feeling')
+	OPTIONS_J = (("J", "J"),("P", "P"),)
+	perception_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS_J, label='Judging/Perceiving')
+
 
 class TwitterHandleField(forms.Form):
 	twitter_handle = forms.CharField(label='', required=None)
 
-class ExtraversionField(forms.Form):
-	OPTIONS = (("E", "E"),("I", "I"),)
-	extraversion_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Extraversion/Introversion')
 
-class IntuitionField(forms.Form):
-	OPTIONS = (("S", "S"),("N", "N"),)
-	intuition_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Sensing/Intuition')
+# class ExtraversionField(forms.Form):
+# 	OPTIONS = (("E", "E"),("I", "I"),)
+# 	extraversion_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Extraversion/Introversion')
 
-class FeelingField(forms.Form):
-	OPTIONS = (("T", "T"),("F", "F"),)
-	feeling_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Thinking/Feeling')
+# class IntuitionField(forms.Form):
+# 	OPTIONS = (("S", "S"),("N", "N"),)
+# 	intuition_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Sensing/Intuition')
 
-class PerceptionField(forms.Form):
-	OPTIONS = (("J", "J"),("P", "P"),)
-	perception_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Judging/Perceiving')
+# class FeelingField(forms.Form):
+# 	OPTIONS = (("T", "T"),("F", "F"),)
+# 	feeling_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Thinking/Feeling')
+
+# class PerceptionField(forms.Form):
+# 	OPTIONS = (("J", "J"),("P", "P"),)
+# 	perception_field = forms.MultipleChoiceField(widget=forms.CheckboxSelectMultiple, choices=OPTIONS, label='Judging/Perceiving')
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .forms import TextInputField, TwitterHandleField, ExtraversionField, IntuitionField, FeelingField, PerceptionField
+from .forms import TextInputField, TwitterHandleField
 
 from bs4 import BeautifulSoup
 import urllib.request
@@ -12,27 +12,34 @@ def home_view(request, *args, **kwargs):	# *args, **kwargs
 	textInputField = TextInputField(request.POST or None)
 	twitterHandleField = TwitterHandleField(request.POST or None)
 
-	extraversionField = ExtraversionField(request.POST or None)
-	intuitionField = IntuitionField(request.POST or None)
-	feelingField = FeelingField(request.POST or None)
-	perceptionField = PerceptionField(request.POST or None)
+	# extraversionField = ExtraversionField(request.POST or None)
+	# intuitionField = IntuitionField(request.POST or None)
+	# feelingField = FeelingField(request.POST or None)
+	# perceptionField = PerceptionField(request.POST or None)
+	# myersBriggsTypesField = MyersBriggsTypesField(request.POST or None)
 
 	twitter_search = False
 
 	if request.method == "POST":
 		if textInputField.is_valid():
 			text_field_data = textInputField.cleaned_data.get('text_field')
+			extraversion_data = textInputField.cleaned_data.get('extraversion_field')
+			intuition_data = textInputField.cleaned_data.get('intuition_field')
+			feeling_data = textInputField.cleaned_data.get('feeling_field')
+			perception_data = textInputField.cleaned_data.get('perception_field')
+			print("Text Field:", text_field_data)
+			print("Extraversion:", extraversion_data)
+			print("Intuition:", intuition_data)
+			print("Feeling:", feeling_data)
+			print("Perception:", perception_data)
+
 			if text_field_data:
 				twitter_search = False
-		if extraversionField.is_valid():
-			extraversion_data = extraversionField.cleaned_data.get('extraversion_field')
-		if intuitionField.is_valid():
-			intuition_data = intuitionField.cleaned_data.get('intuition_field')
-		if feelingField.is_valid():
-			feeling_data = feelingField.cleaned_data.get('feeling_field')
-		if perceptionField.is_valid():
-			perception_data = perceptionField.cleaned_data.get('perception_field')
-			print(perception_data)
+				context = {}
+				return render(request, "results.html", context)
+			else:
+				pass
+
 
 
 		if twitterHandleField.is_valid():
@@ -40,20 +47,23 @@ def home_view(request, *args, **kwargs):	# *args, **kwargs
 			if twitter_field_data:
 				twitter_search = True
 
-
-
+		"""
+		Currently the user can type in values into the boxes and 
+		"""
 
 
 	context = {
 		"textInputField":textInputField,
 		"twitterHandleField":twitterHandleField,
-		"extraversionField":extraversionField,
-		"intuitionField":intuitionField,
-		"feelingField":feelingField,
-		"perceptionField":perceptionField,
-
+		# "myersBriggsTypesField":myersBriggsTypesField,
+		# "extraversionField":extraversionField,
+		# "intuitionField":intuitionField,
+		# "feelingField":feelingField,
+		# "perceptionField":perceptionField,
 
 		"twitter_search":twitter_search,
 
 	}
 	return render(request, "home_view.html", context)
+
+
